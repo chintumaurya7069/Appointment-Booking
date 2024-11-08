2d2ab72e011c36bf5656c3da3cbd875fad1ab558
chintumaurya7069 <127584339+chintumaurya7069@users.noreply.github.com>
2024-11-08 18:23:09 +0530

    Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,27 +5,19 @@ const Contact = () => {
   return (
     <div>
       <h1 className='text-3xl text-center justify-center py-5'>CONTACT US</h1>
-      <div className='flex gap-6 pt-5'>
-        <div>
-          <img className='w-[1500px]' src={assets.contact_image} alt="" />
+      <div className='my-10 flex flex-col md:flex-row justify-center gap-6 pt-5 mb-20'>
+
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+
+        <div className='flex flex-col justify-center items-start gap-6 px-5'>
+          <p className='font-semibold text-lg text-gray-600'> OUR OFFICE</p>
+          <p className='text-gray-500'> RAJ-VICTORIA ADAJAN GAM SURAT <br />Gujarat, India </p>
+          <p className='text-gray-500'>Mobile no. :- 9106496387 <br />Email:chintumaurya7069@gmail.com</p>
+          <p className='font-semibold text-lg text-gray-600'>CAREERS AT PASV</p>
+          <p className='text-gray-500'>learn more about out teams and jobs openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore now</button>
         </div>
-        <div className='px-5'>
-          <div>
-            <p>OUR OFFICE</p>
-            <p>412 raj-victoria <br /> adajan gam surat, Gujarat</p>
-          </div>
-          <div>
-            <p>Tel: +91 9106496387</p>
-            <p>Email: chintumaurya7069@gmail.com</p>
-          </div>
-        </div>
-        <div>
-          <h1>CAREERS AT PASV</h1>
-          <p>Learn more about our teams and job openings.</p>
-        </div>
-        <div>
-          <button>Explore Jobs</button>
-        </div>
+
       </div>
     </div>
   )
